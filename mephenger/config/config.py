d4bf87a13c5790406585c7313e84eda542c8cc09
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-11-14 17:14:13 +0100

    Don't use sysargs for app config

--- mephenger/config/config.py
+++ mephenger/config/config.py
@@ -7,7 +7,6 @@
 
 # Couleurs
 import os
-import sys
 
 MAIN_COLOR = (0.580, 0.533, 0.984)
 R, G, B, = MAIN_COLOR
@@ -15,7 +14,7 @@ BG_COLOR_LEVEL_1 = (R, G, B, 1)
 TEXT_COLOR = (1, 1, 1, 1)
 
 # Chemins
-ROOT_DIR = sys.path[1]
+ROOT_DIR = os.environ["PRJ_ROOT"]
 PUBLIC_DIR = os.path.join(ROOT_DIR, 'public')
 IMG_DIR = os.path.join(PUBLIC_DIR, 'images')
 VIEWS_DIR = os.path.join(PUBLIC_DIR, 'views')
