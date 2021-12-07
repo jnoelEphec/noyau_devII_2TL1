821d3ef856212257ae16d2fc3793bf4002218eef
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-12-07 10:53:14 +0100

    Update the MongoConnector to something usable

--- mephenger/db.py
+++ mephenger/db.py
@@ -1,35 +1,87 @@
 from __future__ import annotations
-from pymongo import MongoClient
 
-from mephenger import config
+from typing import Generator
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+from mephenger import models
 
 
 class MongoConnector:
     """
-        Cette classe permet de créer une connexion vers la base de données.
+    Allow connecting to a mongodb database.
 
-        Veuillez modifier la variable 'certificat_path' avec le chemin vers
-        l'endroit ou se trouve votre certificat.
+    To access the database, use a `with` statement:
 
-        Exemple d'utilisation dans votre code :
-
-        try:
-            with MongoConnector() as connector:
-                collection = connector._db["users"]
-                res = collection.find_one()
-                print(res)
-        except Exception as e:
-            print(e)
+    ```
+    with MongoConnector(uri, cert) as db:
+        my_conv = db.conversations_of_user(my)user)
+    ```
     """
 
-    def __init__(self):
-        client = MongoClient(
-            config.DB_URI, tls=True, tlsCertificateKeyFile=config.DB_CERT
-        )
-        self._db = client['ephecom']
+    def __init__(self, uri: str, cert: str):
+        self._uri = uri
+        self._cert = cert
+        self._db = None
+        # TODO: Use kivy cache
+        self._cache = {"conversations": {}, "messages": {}, "users": {}}
 
     def __enter__(self) -> MongoConnector:
-        return self._db
+        client = MongoClient(
+            self.uri, tls=True, tlsCertificateKeyFile=self.cert
+        )
+        self._db: Database = client["ephecom"]
+        self._conversations: Collection = self._db["conversations"]
+        self._messages: Collection = self._db["messages"]
+        self._users: Collection = self._db["users"]
+        return self
 
     def __exit__(self):
         self._db.close()
+        del self._db
+        self._db = None
+
+    @property
+    def uri(self) -> str:
+        return self._uri
+
+    @property
+    def cert(self) -> str:
+        return self._cert
+
+    def messages_from_conversation(
+        self, conversation: models.Conversation
+    ) -> Generator[models.Message]:
+        """
+        Get an iterator over the messages from given conversation.
+        """
+        for message in self._messages.find(
+                {"conversation": conversation.id}
+        ):
+            message["conversation"] = conversation
+            self._cache["messages"][message["_id"]] = models.Message(**message)
+            yield self._cache["messages"][message["_id"]]
+
+    def conversations_of_user(
+        self, user: models.User
+    ) -> Generator[models.Conversation]:
+        """
+        Get an iterator over the conversations of a user.
+        """
+        for conversation in self._conversations.find({"members": user.id}):
+            missing_members = [
+                u for u in conversation["members"]
+                if u not in self._cache["users"]
+            ]
+            for user in self._users.find({"_id": {"$in": missing_members}}):
+                self._cache["users"][user["_id"]] = models.User(**user)
+            conversation["members"] = [
+                self._cache["users"][u] for u in conversation["members"]
+            ]
+            self._cache["conversations"][conversation["_id"]] = \
+                models.Conversation(**conversation)
+            yield self._cache["conversations"][conversation["_id"]]
+
+    # TODO: Implement more methods
