e6da39fd30dac511b471f8d9c543a790a28abb07
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-12-11 22:23:35 +0100

    [exceptions] Add PermissionDenied and NotAGroup exceptions

--- mephenger/exceptions/__init__.py
+++ mephenger/exceptions/__init__.py
@@ -1,5 +1,5 @@
 from mephenger.exceptions.auth import IncorrectPassword, NotAMember
 from mephenger.exceptions.base import MephengerException
 from mephenger.exceptions.concurrency import TimeoutExpired, WouldBlock
-from mephenger.exceptions.generic import IncompleteItem, NoSuchItem
+from mephenger.exceptions.generic import IncompleteItem, NoSuchItem, Todo
 from mephenger.exceptions.json import NotJsonSerializable
