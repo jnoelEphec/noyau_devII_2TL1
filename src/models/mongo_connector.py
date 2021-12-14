9ac53525066c081dded9724ff2d4b7d7423fe5ed
Morgane Austreelis <morgane.austreelis@gmail.com>
2021-12-14 03:16:09 +0100

    [config.py] Add MONGODB_* config values
    These are then used in `mongo_connector.py`.
    
    This allows customizing the cert path, database name and uri through env variables.

--- src/models/mongo_connector.py
+++ src/models/mongo_connector.py
@@ -21,12 +21,8 @@ class MongoConnector:
     """
 
     def __init__(self):
-        certificat_path = config.ROOT_DIR + "/certif_mongo.pem"
-        uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
-        client = MongoClient(uri,
-                             tls=True,
-                             tlsCertificateKeyFile=certificat_path)
-        self.db = client['ephecom']
+        client = MongoClient(config.MONGODB_URI, **config.MONGODB_CONNECT_OPT)
+        self.db = client[config.MONGODB_DBNAME]
 
     def __enter__(self):
         return self
